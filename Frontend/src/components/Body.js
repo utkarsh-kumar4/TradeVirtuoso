c3f45fc9cdac2111c985ea5f99af37f26ca50a0f
Utkarsh Kumar <87551599+utkarsh-kumar4@users.noreply.github.com>
2024-05-12 11:31:43 +0530

    Create Body.js

---
+++ Frontend/src/components/Body.js
@@ -0,0 +1,41 @@
+import React, { Profiler } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Feed from './Feed';
+import Profile from './Profile';
+import Learn from './Learn';
+
+const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element:<Home/>,
+            children:[
+                {
+                    path:"/",
+                    element:<Feed/>
+                },
+                {
+                    path:"/profile/:id",
+                    element:<Profile/>
+                },
+            ]
+        },
+        {
+            path:"/login",
+            element:<Login/>
+        },
+        {
+            path:"/learn",
+            element:<Learn/>
+        }
+    ])
+    return (
+        <div>
+            <RouterProvider router={appRouter}/>
+        </div>
+    )
+}
+
+export default Body
